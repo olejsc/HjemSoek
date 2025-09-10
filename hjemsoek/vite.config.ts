44489845be3c4a81859b4834fce2bc96a782d1ce
Ole Jakob Schjøth <ole_jakob94@hotmail.com>
2025-09-10 14:14:24 +0200

    style: Clean up HTML indentation and fix icon path in index.html; format Vite config for clarity

--- hjemsoek/vite.config.ts
+++ hjemsoek/vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import tailwindcss  from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// Set base path for GitHub Pages (repo name). In local dev (vite), base isn't used.
+const base = process.env.GITHUB_ACTIONS ? '/HjemSoek/' : '/'
+
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  base,
+  plugins: [react(), tailwindcss()],
 })
